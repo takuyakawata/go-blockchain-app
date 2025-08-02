9d2053c222ff51bb17b9d016c13e1c87494e0832
norishio <131588958+takuyakawata@users.noreply.github.com>
2025-08-03 06:11:32 +0900

    main

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run *.go <pattern>")
+		fmt.Println("Available patterns:")
+		fmt.Println("  1 - Simple Blockchain")
+		fmt.Println("  2 - Blockchain with Proof of Work")
+		fmt.Println("  3 - Blockchain with Persistent Storage")
+		fmt.Println("  4 - Blockchain with CLI Interface")
+		fmt.Println("  5 - Blockchain with Wallet System")
+		fmt.Println("  6 - Blockchain with Transactions and UTXO")
+		return
+	}
+
+	pattern := os.Args[1]
+
+	switch pattern {
+	case "1":
+		RunBlockchainOne()
+	case "2":
+		RunBlockchainTwo()
+	case "3":
+		RunBlockchainThree()
+	case "4":
+		RunBlockchainFour()
+	case "5":
+		RunBlockchainFive()
+	case "6":
+		RunBlockchainSix()
+	default:
+		fmt.Printf("Unknown pattern: %s\n", pattern)
+		fmt.Println("Available patterns: 1, 2, 3, 4, 5, 6")
+	}
+}
